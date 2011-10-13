c82e5b70fc0c2e704a324bcc7a9d642e77ef3875
Mark Kremer <mark@socialreferral.com>
2011-10-13 15:07:52 +0200

    Log a warning when the default url cannot be determined

--- lib/paperclip-defaults.rb
+++ lib/paperclip-defaults.rb
@@ -12,11 +12,16 @@ module Paperclip
   module ClassMethods
     alias_method :has_attached_file_without_defaults, :has_attached_file
     def has_attached_file_with_defaults(name, options = {})
-      if options[:default_asset_url]
-        options[:default_url] = ActionController::Base.new.view_context.asset_path(options[:default_asset_url])
-        options.delete(:default_asset_url)
-      end
       paperclip_options = ::Rails.application.config.paperclip_defaults.merge(options)
+      if paperclip_options[:default_asset_url]
+        begin
+          paperclip_options[:default_url] = ActionController::Base.new.view_context.asset_path(paperclip_options[:default_asset_url])
+        rescue StandardError => e
+          ::Rails.logger.warn("Could not set default asset url for '#{default_options[:default_asset_url]}'")
+        end
+
+        paperclip_options.delete(:default_asset_url)
+      end
       has_attached_file_without_defaults(name, paperclip_options)
     end
     alias_method :has_attached_file, :has_attached_file_with_defaults
