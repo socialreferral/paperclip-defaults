988d386676cf6d8b47734c8cd55a732adef8eb22
Mark Kremer <mark@without-brains.net>
2012-02-23 11:57:35 +0100

    Removed default_asset_url option, you can use default_url instead

--- lib/paperclip-defaults.rb
+++ lib/paperclip-defaults.rb
@@ -13,16 +13,6 @@ module Paperclip
     alias_method :has_attached_file_without_defaults, :has_attached_file
     def has_attached_file_with_defaults(name, options = {})
       paperclip_options = ::Rails.application.config.paperclip_defaults.merge(options)
-      if paperclip_options[:default_asset_url]
-        begin
-          default_asset_url = paperclip_options[:default_asset_url]
-          paperclip_options[:default_url] = ->(attachment) {ActionController::Base.new.view_context.asset_path(default_asset_url.respond_to?(:call) ? default_asset_url.call(attachment) : default_asset_url)}
-        rescue StandardError => e
-          ::Rails.logger.warn("Could not set default asset url for '#{paperclip_options[:default_asset_url]}'")
-        end
-
-        paperclip_options.delete(:default_asset_url)
-      end
       has_attached_file_without_defaults(name, paperclip_options)
     end
     alias_method :has_attached_file, :has_attached_file_with_defaults
--- test/test_paperclip-defaults.rb
+++ test/test_paperclip-defaults.rb
@@ -17,36 +17,6 @@ class TestPaperclipDefaults < MiniTest::Unit::TestCase
     assert_equal expect, model.attachment_definitions[:image1]
   end
 
-  def test_default_asset_url_will_use_action_controller_view_context_to_decide_default_url
-    ActionController::Base.stubs(:new).returns(mock('new_action_controller'))
-    ActionController::Base.new.stubs(:view_context).returns(mock('view_context'))
-    ActionController::Base.new.view_context.stubs(:asset_path).returns('http://www.example.com/unknown.png')
-
-    model = Class.new(ActiveRecord::Base) do
-      has_attached_file :image1, default_asset_url: "unknown.png"
-    end
-
-    expect = {validations: [], default_url: "http://www.example.com/unknown.png"}
-    assert_equal [], model.attachment_definitions[:image1][:validations]
-    assert_equal Proc, model.attachment_definitions[:image1][:default_url].class
-    assert_equal "http://www.example.com/unknown.png", model.attachment_definitions[:image1][:default_url].call(nil)
-  end
-
-  def test_default_asset_url_with_proc_will_make_use_of_it_each_time
-    ActionController::Base.stubs(:new).returns(mock('new_action_controller'))
-    ActionController::Base.new.stubs(:view_context).returns(mock('view_context'))
-    ActionController::Base.new.view_context.stubs(:asset_path).returns('http://www.example.com/unknown.png')
-
-    model = Class.new(ActiveRecord::Base) do
-      has_attached_file :image1, default_asset_url: ->(attachment){ "UNKNOWN.PNG".downcase }
-    end
-
-    expect = {validations: [], default_url: "http://www.example.com/unknown.png"}
-    assert_equal [], model.attachment_definitions[:image1][:validations]
-    assert_equal Proc, model.attachment_definitions[:image1][:default_url].class
-    assert_equal "http://www.example.com/unknown.png", model.attachment_definitions[:image1][:default_url].call(nil)
-  end
-
   def test_defaults_will_not_overwrite_explicit_options
     Rails.application.config.stubs(:paperclip_defaults).returns({storage: :fog})
     model = Class.new(ActiveRecord::Base) do
